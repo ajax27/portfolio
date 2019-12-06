cb8881878f69218a75bfe7e5319058857c751345
Shaun Collins <shaun.c16@hotmail.co.uk>
2019-12-06 17:09:32 +0000

    make it better

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
-  "process.env.NAMESPACE": "https://ajax27.herokuapp.com"
+  NAMESPACE: "https://ajax27.herokuapp.com",
+  CLIENT_ID: "E9pwenTnQjEN60yivBNLJrZB6D7J2IVp"
 };
